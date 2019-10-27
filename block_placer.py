7d7d1011041178ee7f66cbceb277b41fea3f5a52
David Legg <davey.legg@gmail.com>
2019-10-26 20:33:29 -0700

    Added simulated mission capability, shaped reward function.

--- block_placer.py
+++ block_placer.py
@@ -36,9 +36,14 @@ else:
     print = functools.partial(print, flush=True)
 
 MODEL_NAME     = 'block_placer'
-VERSION_NUMBER = '1.1'
+VERSION_NUMBER = '2.0'
 
 MAX_RETRIES = 10
+
+NUM_EPISODES     = 1000
+MAX_EPISODE_TIME = 20
+INITIAL_EPSILON  = 0.5
+EPSILON_DECAY    = 0.995
 
 # All coordinates are in Minecraft's global coordinate system.
 ARENA_WIDTH  = 5 # X-direction
@@ -106,7 +111,7 @@ If training=True, sets overclocking and deactivates rendering.'''
               </ServerInitialConditions>
               <ServerHandlers>
                   <FlatWorldGenerator generatorString="3;5*minecraft:bedrock;1;" forceReset="1"/>
-                  <ServerQuitFromTimeUp timeLimitMs="20000"/>
+                  <ServerQuitFromTimeUp timeLimitMs="{server_timeout}"/>
                   <ServerQuitWhenAnyAgentFinishes/>
                 </ServerHandlers>
               </ServerSection>
@@ -129,7 +134,6 @@ If training=True, sets overclocking and deactivates rendering.'''
                   </ObservationFromGrid>
                   <DiscreteMovementCommands/>
                   <MissionQuitCommands/>
-                  <RewardForSendingCommand reward="-1" />
                 </AgentHandlers>
               </AgentSection>
             </Mission>'''.format(
@@ -139,11 +143,12 @@ If training=True, sets overclocking and deactivates rendering.'''
                     start_y = start_y + OFFSET_Y - 1, # -1 corrects for a mismatch in the way MC positions characters vs. how it reads the position back
                     start_z = start_z + OFFSET_Z,
                     arena_x1 = ANCHOR_X, arena_x2 = ANCHOR_X - 1 + ARENA_WIDTH,
-                    arena_y1 = ANCHOR_Y, arena_y2 = ANCHOR_Y - 1 + ARENA_HEIGHT,
+                    arena_y1 = ANCHOR_Y - 1, arena_y2 = ANCHOR_Y - 1 + ARENA_HEIGHT,
                     arena_z1 = ANCHOR_Z, arena_z2 = ANCHOR_Z - 1 + ARENA_LENGTH,
                     border_x1 = ANCHOR_X - 1, border_x2 = ANCHOR_X + ARENA_WIDTH,
                     border_y1 = ANCHOR_Y - 2, border_y2 = ANCHOR_Y - 1,
-                    border_z1 = ANCHOR_Z - 1, border_z2 = ANCHOR_Z + ARENA_LENGTH
+                    border_z1 = ANCHOR_Z - 1, border_z2 = ANCHOR_Z + ARENA_LENGTH,
+                    server_timeout = 1000*MAX_EPISODE_TIME
                 )
     ACTION_DELAY = (0.2/OVERCLOCK_FACTOR if training else 0.2)
 
@@ -159,23 +164,31 @@ If training=True, sets overclocking and deactivates rendering.'''
         exit(0)
 
 class WorldModel:
-    def __init__(self, blueprint):
+    def __init__(self, blueprint, simulated=False):
         self._bp       = blueprint
-        self._world    = None
         self._str_type = '<U{}'.format(max(len(s) for s in INPUTS))
         self._rot_bp   = self._bp
         self._old_num_complete    = 0
         self._old_num_incomplete  = 0
         self._old_num_superfluous = 0
+        self._attacked_floor = False
+        if not simulated:
+            # Wait for update() from Minecraft
+            self._world = None
+        else:
+            # Build world with random agent start position
+            self._world = np.full((ARENA_LENGTH, ARENA_HEIGHT, ARENA_WIDTH), fill_value='air', dtype=self._str_type)
+            self._world[np.random.randint(ARENA_LENGTH),0,np.random.randint(ARENA_WIDTH)] = 'agent'
 
     def update(self, raw_obs):
+        '''Used when hooked up to Minecraft.'''
         if self._world is not None:
             self._old_num_complete    = self.num_complete()
             self._old_num_incomplete  = self.num_incomplete()
             self._old_num_superfluous = self.num_superfluous()
 
         raw_world = np.array( raw_obs["world_grid"], dtype=self._str_type )
-        world = np.transpose(np.reshape(raw_world, (ARENA_WIDTH, ARENA_LENGTH, ARENA_HEIGHT)), (2, 0, 1))
+        world = np.transpose(np.reshape(raw_world, (ARENA_WIDTH, ARENA_LENGTH, ARENA_HEIGHT+1)), (2, 0, 1))
         agent_yaw = raw_obs['Yaw']
         agent_x = int(raw_obs['XPos'] - OFFSET_X)
         agent_y = int(raw_obs['YPos'] - OFFSET_Y)
@@ -185,8 +198,78 @@ class WorldModel:
             0 <= agent_z < world.shape[2] ):
             world[agent_x, agent_y, agent_z] = 'agent'
         # Rotate world and blueprint to be agent-facing
-        self._world  = np.rot90(world,    k=-int(np.round(agent_yaw/90)), axes=(0,2))
+        extended_world = np.rot90(world,    k=-int(np.round(agent_yaw/90)), axes=(0,2))
+        self._world  = extended_world[:,1:,:]
+        self._attacked_floor = (extended_world[:,0,:] == 'air').any()
         self._rot_bp = np.rot90(self._bp, k=-int(np.round(agent_yaw/90)), axes=(0,2))
+
+    def simulate(self, action):
+        '''Used instead of connecting to Malmo, for efficient training.'''
+        if action == "jumpmove 1":
+            # Find agent:
+            agent_pos = tuple(self.agent_position())
+            if agent_pos[2] >= self._world.shape[2] - 1:
+                # Agent is at the edge of the world, facing out. Assume this makes agent jump out of world
+                self._world[agent_pos] = 'air'
+                return
+            # Determine non-air blocks in front of agent
+            #   agent position + 1 in z dir, column from 2 above agent (for head clearance) down
+            in_front = (self._world[agent_pos[0], :agent_pos[1]+3, agent_pos[2]+1] != 'air')
+            if (not in_front[-2:].any() or
+                (agent_pos[2] == self._world.shape[1] - 2 and not in_front[-1]) or
+                (agent_pos[2] == self._world.shape[1] - 1)):
+                # Clearance to jump; compute where we land, as 1 above top-most non-air cell:
+                new_agent_pos = (agent_pos[0], (np.where(in_front)[0][-1]+1 if in_front.any() else 0), agent_pos[2]+1)
+                self._world[agent_pos]     = 'air'
+                self._world[new_agent_pos] = 'agent'
+            # Else, no room to jump. Action is a no-op
+        elif action == "turn 1":
+            self._world  = np.rot90(self._world,  k=-1, axes=(0,2))
+            self._rot_bp = np.rot90(self._rot_bp, k=-1, axes=(0,2))
+        elif action == "turn -1":
+            self._world  = np.rot90(self._world,  k= 1, axes=(0,2))
+            self._rot_bp = np.rot90(self._rot_bp, k= 1, axes=(0,2))
+        elif action == "use":
+            # Find agent:
+            agent_pos = self.agent_position()
+            if ((agent_pos[2] >= self._world.shape[2] - 1) or
+                (self._world[agent_pos[0], agent_pos[1], agent_pos[2] + 1] != 'air')):
+                # Agent is at the edge of the world, facing out, or is staring at a block.
+                return
+            # Determine all non-air blocks in agent's line-of-construction, to build on top of
+            in_front = (self._world[agent_pos[0], max(0, agent_pos[1]-4):agent_pos[1], agent_pos[2]+1] != 'air')
+            if in_front.any():
+                # We're looking at a solid block, so we can place a block on top of it!
+                new_block_y = agent_pos[1] - np.where(np.flip(in_front))[0][0]
+                self._world[agent_pos[0], new_block_y, agent_pos[2]+1] = 'stone'
+            elif agent_pos[1] <= 4:
+                # There was no block to construct on, but we were low enough to see the floor.
+                # Place a block on the floor instead
+                self._world[agent_pos[0], 0, agent_pos[2]+1] = 'stone'
+            # Else, can only see air in line-of-construction, so cannot construct!
+        elif action == "attack":
+            # Find agent:
+            agent_pos = self.agent_position()
+            if agent_pos[2] >= self._world.shape[2] - 1:
+                # Agent is at the edge of the world, facing out. Don't bother tracking this action.
+                return
+            # Determine all air blocks in agents line-of-attack
+            in_front = (self._world[agent_pos[0], max(0, agent_pos[1]-4):agent_pos[1]+1, agent_pos[2]+1] != 'air')
+            if in_front.any():
+                # We're looking at a solid block, so we can attack it
+                attacked_block_y = agent_pos[1] - np.where(np.flip(in_front))[0][0] 
+                self._world[agent_pos[0], attacked_block_y, agent_pos[2]+1] = 'air'
+            elif agent_pos[1] <= 4:
+                # There was no block to attack, but we were low enough to see the floor.
+                # In the real MC, this would attack the floor, which we don't allow
+                self._attacked_floor = True
+            # Else, can only see air in line-of-attack, so cannot attack!
+        else:
+            print('ERROR: Illegal action {} specified.'.format(action))
+            exit(1)
+
+    def agent_position(self):
+        return np.ravel(np.where(self._world == 'agent'))
 
     def get_observation(self):
         return np.array([self._rot_bp, self._world])
@@ -204,15 +287,44 @@ class WorldModel:
         '''Returns true if world is uninitialized or agent is present in world model.'''
         return (self._world is None) or (self._world == 'agent').any()
 
+    def agent_attacked_floor(self):
+        return self._attacked_floor
+
     def mission_complete(self):
         return self.num_incomplete() == 0 and self.num_superfluous() == 0
 
+    def distance_to_incomplete(self, default=None):
+        '''Returns the minimum straight-line distance in Minecraft units from agent to a block specified in the blueprint which doesn't match the world'''
+        # Tuple of arrays (X, Y, Z) s.t. (xj, yj, zj) is the j-th incomplete block in the blueprint
+        incomplete = np.array(np.where( (self._rot_bp != self._world) & (self._rot_bp != 'air') & (self._world != 'agent') ))
+        # Compute agent position:
+        agent_pos = self.agent_position()
+        if incomplete.size == 0 or agent_pos.size == 0:
+            return default
+        # Compute distance to each block using some numpy magic:
+        #   Manually broadcasts agent_pos to line up with incomplete
+        #   Then, computes difference of coordinates, and element-wise squares them
+        #   Next, sums acroos coords of each sample, to give a 1D output vector
+        #   Finally, takes min of these distances as output
+        return np.sum( (incomplete - np.repeat(agent_pos.reshape((-1, 1)), incomplete.shape[1]))**2, axis=0 ).min()
+
     def reward(self):
+        if self.agent_attacked_floor() or not self.agent_in_arena():
+            return -1
+        if self.mission_complete():
+            return 1
+        # Compute the farthest an agent could theoretically be from the nearest blueprint block: opposite world corner
+        max_dist = np.sum(np.array(self._world.shape)**2)
         return (
-            (  10 * (self.num_complete() - self._old_num_complete) ) +
-            ( -10 * (self.num_superfluous() - self._old_num_superfluous) ) +
-            (-200 * (not self.agent_in_arena()) ) +
-            ( 200 * (self.mission_complete()) )
+            # Use a default=1 on distance_to_incomplete
+            #   so that agent optimizes that part all blocks are complete
+            #   Avoids possibility of dancing around the last incomplete block to gain reward
+            (  0.1 * (1 - abs((1/max_dist) - self.distance_to_incomplete(default=1))**0.4) ) +
+            # Reward actually placing necessary blocks, and penalize placing superfluous ones
+            #   This also penalizes removing necessary blocks, and rewards removing superfluous ones
+            #   That second function avoids being able to place the same block over and over to rack up rewards
+            (  0.5 * (self.num_complete() - self._old_num_complete) ) +
+            ( -0.5 * (self.num_superfluous() - self._old_num_superfluous) )
             )
 
 class RLearner:
@@ -222,7 +334,7 @@ class RLearner:
         self._prediction_network = Sequential([
             # Take in the blueprint as desired and the state of the world, in the same shape as the blueprint
             InputLayer(input_shape=(2, *BLUEPRINT_OH.shape)),
-            # Reduce incoming 6D tensor to 5D by merging channels (dim 1) and one-hot (dim 6):
+            # Reduce incoming 6D tensor to 5D by merging channels (dim 1) and one-hot (dim 5):
             Permute((1, 5, 2, 3, 4)),
             Reshape((-1, *BLUEPRINT.shape)),
             # Convolve each input, treating the blueprint and world state as separate channels
@@ -244,8 +356,8 @@ class RLearner:
 
         self._target_update_frequency = 20
         self._iters_since_target_update = 0
-        self._epsilon_decay = 0.995
-        self._epsilon = 0.90 * (self._epsilon_decay**(self.start_epoch + 1))
+        self._epsilon_decay = EPSILON_DECAY
+        self._epsilon = INITIAL_EPSILON * (self._epsilon_decay**(self.start_epoch + 1))
         self._discount = 0.95
         self._last_obs = None
         self._last_action = None
@@ -390,14 +502,42 @@ def run_mission(model, display=None):
 
     return total_reward
 
-def train_model(model, epochs, initial_epoch=0, display=None):
+def run_simulated_mission(model, display=None):
+    print("Simulated mission running.")
+
+    world_model  = WorldModel(BLUEPRINT, simulated=True)
+    ticks_left   = 5*MAX_EPISODE_TIME
+    total_reward = 0
+    current_r    = 0
+
+    while (ticks_left > 0 and
+           world_model.agent_in_arena() and
+           not world_model.mission_complete()):
+        current_r = world_model.reward()
+        action = model.act(current_r, world_model.get_observation())
+        if display is not None:
+            display.update(world_model)
+        total_reward += current_r
+        world_model.simulate(action)
+
+    # Collect last reward, and give to model, then end the mission
+    current_r = world_model.reward()
+    model.act(current_r, world_model.get_observation())
+    total_reward += current_r
+    model.mission_ended()
+    print()
+    print("Simulated mission ended")
+
+    return total_reward
+
+def train_model(model, epochs, initial_epoch=0, display=None, simulated=False):
     best_reward = None
     for epoch_num in range(initial_epoch, epochs):
         if epoch_num % 10 == 0:
             build_world(training=True, randomize_start_xz=True)
         print('Epoch {}/{}'.format(epoch_num, epochs))
         print('Current Epsilon: {}'.format(model._epsilon))
-        reward = run_mission(model, display=display)
+        reward = (run_simulated_mission(model, display=display) if simulated else run_mission(model, display=display))
         print('Total reward:', reward)
         if best_reward is None or reward > best_reward or epoch_num % 10 == 0:
             model.save('epoch_{:03d}.reward_{:03d}'.format(epoch_num, int(reward)))
@@ -408,6 +548,6 @@ if __name__ == '__main__':
     build_world(training=True, randomize_start_xz=True)
     model = RLearner()
     disp  = Display(model)
-    train_model(model, 1000, initial_epoch=model.start_epoch, display=disp)
+    train_model(model, NUM_EPISODES, initial_epoch=model.start_epoch, display=disp, simulated=True)
 
 
